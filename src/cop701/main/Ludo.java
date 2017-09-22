9801fccfb9f7cc5650189cc47bd94df2d8541951
Chak Fai Yuen <mcs172525@iitd.ac.in>
2017-09-23 00:57:30 +0530

    Interactive UI for testing

--- src/cop701/main/Ludo.java
+++ src/cop701/main/Ludo.java
@@ -1,10 +1,30 @@
 package cop701.main;
 
+import java.util.Scanner;
+
 public class Ludo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		manualMode();
 	}
+	
+	public static void manualMode() {
+		new LudoSimpleInteractiveUI();
+		Scanner s = new Scanner(System.in);
+		while (s.hasNextLine()) {
+			String str = s.nextLine();
+			System.err.println("[bot] " + str);
+		}
+		s.close();
+	}
+	
+	public static void submitText(String input) {
+		System.err.println("[input] " + input);
+		System.out.println(input);
+		System.out.flush();
+	}
+	
 
 }
---
+++ src/cop701/main/LudoSimpleInteractiveUI.java
@@ -0,0 +1,40 @@
+package cop701.main;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+
+public class LudoSimpleInteractiveUI {
+
+	public LudoSimpleInteractiveUI() {
+		buildUI();
+	}
+
+	public static void buildUI() {
+		JFrame f = new JFrame("Ludo");
+		JButton b1 = new JButton("Submit");
+		JTextArea t1 = new JTextArea("");
+		JLabel l1 = new JLabel("Input to server");
+		b1.setBounds(20, 20, 95, 30);
+		l1.setBounds(10, 80, 150, 30);
+		t1.setBounds(150, 80, 300, 30);
+		f.add(b1);
+		f.add(t1);
+		f.add(l1);
+		f.setSize(500, 200);
+		f.setLayout(null);
+		f.setVisible(true);
+		b1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String text = t1.getText();
+				t1.setText("");
+				Ludo.submitText(text);
+			}
+		});
+	}
+
+}
